a7061bc6207fd9abc6ef676ffd1ac3bee36ce356
Mark Jones <mark@jonesgroup.co.uk>
2018-02-07 10:29:35 +0000

    Rank:7195 Timestamp:1517998982
    First ever successful submission. Pretty dire, but then again, I expected that.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,9 +1,7 @@
 import pandas
 import os
-import matplotlib.pyplot as plt
-import seaborn
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from utils.filestuff import source_path
 from sklearn.svm import SVC
 
@@ -11,6 +9,8 @@ pandas.set_option('display.width', 1000)
 
 CROSS_VALIDATION_SPLITS = 100
 CROSS_VALIDATION_TEST_SIZE = 0.1
+
+doc_path = os.path.join(source_path(__file__), "doc")
 
 
 def age_categorisation(df):
@@ -64,38 +64,58 @@ def pre_processing(df):
     return df
 
 
-def main():
-    doc_path = os.path.join(source_path(__file__), "doc")
+def train():
     training_data_file = os.path.join(doc_path, "train.csv")
-    training_df = pandas.read_csv(training_data_file)
-
-    test_data_file = os.path.join(doc_path, "test.csv")
-    test_df = pandas.read_csv(test_data_file)
-
-    training_df = pre_processing(training_df)
-    test_df = pre_processing(test_df)
+    training_df = pre_processing(pandas.read_csv(training_data_file))
 
     # TODO - stratified cross validation to go here ...
     # TODO - ... followed by a proper pipelining
 
-    training_features, \
-    validation_features, \
-    training_survival, \
-    validation_survival = train_test_split(training_df.drop(['Survived'], axis=1),
-                                           training_df.Survived,
-                                           test_size=CROSS_VALIDATION_TEST_SIZE)
+    x_training, \
+    x_validation, \
+    y_training, \
+    y_validation = train_test_split(training_df.drop(['Survived'], axis=1),
+                                    training_df.Survived,
+                                    test_size=CROSS_VALIDATION_TEST_SIZE)
 
     '''print(training_features)
     print(validation_features)
     print(training_survival)
     print(validation_survival)'''
 
-    linear_svm_classifier = SVC(kernel='linear').fit(training_features, training_survival)
-    linear_svm_classifier_predictions = linear_svm_classifier.predict(validation_features)
-    print(linear_svm_classifier.score(training_features, training_survival),
-          linear_svm_classifier.score(validation_features, validation_survival))
+    linear_svm_classifier = SVC(kernel='linear').fit(x_training, y_training)
+    linear_svm_classifier_predictions = linear_svm_classifier.predict(x_validation)
+    '''print(linear_svm_classifier.score(training_features, training_survival),
+          linear_svm_classifier.score(validation_features, validation_survival))'''
 
-    return linear_svm_classifier.score(validation_features, validation_survival)
+    precision = precision_score(y_validation, linear_svm_classifier_predictions)
+    recall = recall_score(y_validation, linear_svm_classifier_predictions)
+    accuracy = accuracy_score(y_validation, linear_svm_classifier_predictions)
+    f1 = f1_score(y_validation, linear_svm_classifier_predictions)
+
+    best_classifier = linear_svm_classifier
+
+    return best_classifier, accuracy, precision, recall, f1
+
+
+def evaluate(classifier):
+    test_data_file = os.path.join(doc_path, "test.csv")
+    test_df = pandas.read_csv(test_data_file)
+    processed_df = pre_processing(test_df)
+
+    predictions = classifier.predict(processed_df)
+    print(predictions)
+
+    with open(os.path.join(doc_path, "submit001.csv"), 'w') as f:
+        f.write("PassengerId,Survived\n")
+        for i in range(0, len(predictions)):
+            f.write("{0},{1}\n".format(test_df[['PassengerId']].values[i][0], predictions[i]))
+
 
 if __name__ == "__main__":
-    main()
+    best_classifier, accuracy, precision, recall, f1 = train()
+    print("Precision", precision)
+    print("Recall", recall)
+    print("Accuracy", accuracy)
+    print("f1", f1)
+    evaluate(best_classifier)
