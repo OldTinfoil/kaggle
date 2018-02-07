819a23a6200d2e4d5fd27335e80cb52de9da7cc0
Mark Jones <mark@jonesgroup.co.uk>
2018-02-07 10:50:15 +0000

    Rank:7193 Timestamp:1518000588
    Didn't think I would see much improvement. Mostly used to cherry pick the best classifier of its type.

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -2,15 +2,19 @@ import pandas
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from utils.filestuff import source_path
 from sklearn.svm import SVC
+from utils.misc import EmptyObject
+from utils.filestuff import source_path
 
 pandas.set_option('display.width', 1000)
 
 CROSS_VALIDATION_SPLITS = 100
 CROSS_VALIDATION_TEST_SIZE = 0.1
 
+SHUFFLES = 100
+
 doc_path = os.path.join(source_path(__file__), "doc")
+submission_path = os.path.join(source_path(__file__), "submissions")
 
 
 def age_categorisation(df):
@@ -65,37 +69,63 @@ def pre_processing(df):
 
 
 def train():
+    best_classifier = EmptyObject()
+    best_classifier.f1 = 0
+    best_classifier.precision = 0
+    best_classifier.recall = 0
+    best_classifier.accuracy = 0
+
+    precision_sum = recall_sum = accuracy_sum = f1_sum = 0
+
     training_data_file = os.path.join(doc_path, "train.csv")
     training_df = pre_processing(pandas.read_csv(training_data_file))
 
     # TODO - stratified cross validation to go here ...
     # TODO - ... followed by a proper pipelining
 
-    x_training, \
-    x_validation, \
-    y_training, \
-    y_validation = train_test_split(training_df.drop(['Survived'], axis=1),
-                                    training_df.Survived,
-                                    test_size=CROSS_VALIDATION_TEST_SIZE)
+    for _ in range(0,SHUFFLES):
+        x_training, \
+        x_validation, \
+        y_training, \
+        y_validation = train_test_split(training_df.drop(['Survived'], axis=1),
+                                        training_df.Survived,
+                                        test_size=CROSS_VALIDATION_TEST_SIZE)
 
-    '''print(training_features)
-    print(validation_features)
-    print(training_survival)
-    print(validation_survival)'''
+        '''print(training_features)
+        print(validation_features)
+        print(training_survival)
+        print(validation_survival)'''
 
-    linear_svm_classifier = SVC(kernel='linear').fit(x_training, y_training)
-    linear_svm_classifier_predictions = linear_svm_classifier.predict(x_validation)
-    '''print(linear_svm_classifier.score(training_features, training_survival),
-          linear_svm_classifier.score(validation_features, validation_survival))'''
+        linear_svm_classifier = SVC(kernel='linear').fit(x_training, y_training)
+        linear_svm_classifier_predictions = linear_svm_classifier.predict(x_validation)
+        '''print(linear_svm_classifier.score(training_features, training_survival),
+              linear_svm_classifier.score(validation_features, validation_survival))'''
 
-    precision = precision_score(y_validation, linear_svm_classifier_predictions)
-    recall = recall_score(y_validation, linear_svm_classifier_predictions)
-    accuracy = accuracy_score(y_validation, linear_svm_classifier_predictions)
-    f1 = f1_score(y_validation, linear_svm_classifier_predictions)
+        precision = precision_score(y_validation, linear_svm_classifier_predictions)
+        recall = recall_score(y_validation, linear_svm_classifier_predictions)
+        accuracy = accuracy_score(y_validation, linear_svm_classifier_predictions)
+        f1 = f1_score(y_validation, linear_svm_classifier_predictions)
 
-    best_classifier = linear_svm_classifier
+        # No penalty in false positives or false negatives in our data set, so just use f1 as our indicating metric
 
-    return best_classifier, accuracy, precision, recall, f1
+        precision_sum += precision
+        recall_sum += recall
+        accuracy_sum += accuracy
+        f1_sum += f1
+
+        if best_classifier.f1 < f1:
+            best_classifier = linear_svm_classifier
+            best_classifier.f1 = f1
+            best_classifier.precision = precision
+            best_classifier.recall = recall
+            best_classifier.accuracy = accuracy
+
+    f1_avg = f1_sum/SHUFFLES
+    precision_avg = precision_sum / SHUFFLES
+    recall_avg = recall_sum / SHUFFLES
+    accuracy_avg = accuracy_sum / SHUFFLES
+
+    return best_classifier, accuracy_avg, precision_avg, recall_avg, f1_avg
 
 
 def evaluate(classifier):
@@ -104,9 +134,9 @@ def evaluate(classifier):
     processed_df = pre_processing(test_df)
 
     predictions = classifier.predict(processed_df)
-    print(predictions)
+    #print(predictions)
 
-    with open(os.path.join(doc_path, "submit001.csv"), 'w') as f:
+    with open(os.path.join(submission_path, "submit002.csv"), 'w') as f:
         f.write("PassengerId,Survived\n")
         for i in range(0, len(predictions)):
             f.write("{0},{1}\n".format(test_df[['PassengerId']].values[i][0], predictions[i]))
@@ -114,8 +144,10 @@ def evaluate(classifier):
 
 if __name__ == "__main__":
     best_classifier, accuracy, precision, recall, f1 = train()
-    print("Precision", precision)
-    print("Recall", recall)
-    print("Accuracy", accuracy)
-    print("f1", f1)
+    print("Metric", "Avg", "Best")
+    print("Precision", precision, best_classifier.precision)
+    print("Recall", recall, best_classifier.recall)
+    print("Accuracy", accuracy, best_classifier.accuracy)
+    print("f1", f1, best_classifier.f1)
+
     evaluate(best_classifier)
