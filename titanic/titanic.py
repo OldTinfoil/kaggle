0bfe1ae0ff5f3ccc5eb571e357972920077ab543
Mark Jones <mark@jonesgroup.co.uk>
2018-02-07 12:57:32 +0000

    Polynomial SVC is borked - also added embarkation port and age categories to the classifier - appears to make no difference to my score. Hm

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,11 +1,14 @@
 import pandas
 import os
 import pickle
+import json
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.svm import SVC
-from utils.misc import EmptyObject
 from utils.filestuff import source_path
+
+# ie - human readable version of: wot Mark tried today
+NOTE = "Dummy encoded sex/port/category-age classifier with unknowns allowed"
 
 pandas.set_option('display.width', 1000)
 
@@ -14,16 +17,17 @@ CROSS_VALIDATION_TEST_SIZE = 0.1
 
 SHUFFLES = 1000
 
-SVM_KERNELS = ['linear', 'polynomial', 'rbf', 'sigmoid']
+SVM_KERNELS = ['linear', 'poly', 'rbf', 'sigmoid']
 
 doc_path = os.path.join(source_path(__file__), "doc")
 submission_path = os.path.join(source_path(__file__), "submissions")
 
 
 class ClassifierAnalysis:
-    def __init__(self, classifier, accuracy, precision, recall, f1, average_accuracy, average_prediction,
+    def __init__(self, classifier, flavour, accuracy, precision, recall, f1, average_accuracy, average_prediction,
                  average_recall, average_f1, note=""):
         self.classifier = classifier
+        self.flavour = flavour
         self.accuracy = accuracy
         self.precision = precision
         self.recall = recall
@@ -33,6 +37,9 @@ class ClassifierAnalysis:
         self.average_recall = average_recall
         self.average_f1 = average_f1
         self.note = note
+
+    def get_description(self):
+        return str(type(self.classifier)) + "----" + self.flavour
 
 
 def age_categorisation(df):
@@ -50,8 +57,8 @@ def age_categorisation(df):
     df['Age_Adult'] = [1 if 20 <= age < 40 else 0 for age in df['Age']]
     df['Age_Midlife'] = [1 if 40 <= age < 60 else 0 for age in df['Age']]
     df['Age_Senior'] = [1 if 60 <= age < 1000 else 0 for age in df['Age']]
-    df['Age_Unknown'] = [0 if age is None or 0 <= age < 1000 else 1 for age in df['Age']]
-    df.drop(['Age'], axis=1)
+    df['Age_Unknown'] = [0 if 0 <= age <= 1000 else 1 for age in df['Age']]
+    df = df.drop(['Age'], axis=1)
     return df
 
 
@@ -76,13 +83,12 @@ def sex_categorisation(df):
 
 def pre_processing(df):
     # Let's perform dummy encoding for our categorical data
-    # df = age_categorisation(df)
-    # df = port_categorisation(df)
+    df = age_categorisation(df)
+    df = port_categorisation(df)
     df = sex_categorisation(df)
 
     # drop unused metrics (for now at least)
-    df = df.drop(['PassengerId', 'Pclass', 'Name', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked'],
-                 axis=1)
+    df = df.drop(['PassengerId', 'Pclass', 'Name', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin'], axis=1)
     return df
 
 
@@ -94,18 +100,21 @@ def train():
     training_data_file = os.path.join(doc_path, "train.csv")
     training_df = pre_processing(pandas.read_csv(training_data_file))
 
+    '''with pandas.option_context('display.max_rows', None):
+        print(training_df)'''
+
     # TODO - stratified cross validation to go here ...
     # TODO - ... followed by a proper pipelining
 
     for kernel in SVM_KERNELS:
-        best_classifier_candidate = ClassifierAnalysis(None, 0, 0, 0, 0, 0, 0, 0, 0)
+        best_classifier_candidate = ClassifierAnalysis(None, "Bland", 0, 0, 0, 0, 0, 0, 0, 0)
         for _ in range(0, SHUFFLES):
             x_training, x_validation, y_training, y_validation = train_test_split(training_df.drop(['Survived'],
                                                                                                    axis=1),
                                                                                   training_df.Survived,
                                                                                   test_size=CROSS_VALIDATION_TEST_SIZE)
 
-            linear_svm_classifier = SVC(kernel='linear').fit(x_training, y_training)
+            linear_svm_classifier = SVC(kernel=kernel).fit(x_training, y_training)
             linear_svm_classifier_predictions = linear_svm_classifier.predict(x_validation)
             '''print(linear_svm_classifier.score(training_features, training_survival),
                   linear_svm_classifier.score(validation_features, validation_survival))'''
@@ -123,7 +132,8 @@ def train():
             f1_sum += f1
 
             if best_classifier_candidate.f1 < f1:
-                best_classifier_candidate = ClassifierAnalysis(linear_svm_classifier, accuracy, precision, recall, f1, 0, 0, 0, 0)
+                best_classifier_candidate = ClassifierAnalysis(linear_svm_classifier, kernel, accuracy, precision,
+                                                               recall, f1, 0, 0, 0, 0)
 
         f1_avg = f1_sum / SHUFFLES
         precision_avg = precision_sum / SHUFFLES
@@ -134,15 +144,16 @@ def train():
         best_classifier_candidate.average_precision = precision_avg
         best_classifier_candidate.average_recall = recall_avg
         best_classifier_candidate.average_f1 = f1_avg
-        best_classifier_candidate.note = kernel
+        best_classifier_candidate.flavour = kernel
+        best_classifier_candidate.note = NOTE
 
         classifiers.append(best_classifier_candidate)
 
     print("Kernel", "f1")
-    print(classifiers[0].note, classifiers[0].f1)
+    print(classifiers[0].get_description(), classifiers[0].f1)
     best_svm_classifier = classifiers[0]
     for classifier in classifiers[1:]:
-        print(classifier.note, classifier.f1)
+        print(classifier.get_description(), classifier.f1)
         if classifier.f1 > best_svm_classifier.f1:
             best_svm_classifier = classifier
 
@@ -165,7 +176,22 @@ def evaluate(classifier, basename):
 
 def store_classifier(classifier, basename):
     with open(os.path.join(submission_path, ".".join([basename, "classifier"])), 'wb') as f:
-        pickle.dump(classifier, f)
+        pickle.dump(classifier.classifier, f)
+
+    json_dict = {}
+    with open(os.path.join(submission_path, ".".join([basename, "summary"])), 'w') as f:
+        json_dict["classifier"] = classifier.get_description()
+        json_dict["accuracy"] = classifier.accuracy
+        json_dict["precision"] = classifier.precision
+        json_dict["recall"] = classifier.recall
+        json_dict["f1"] = classifier.f1
+        json_dict["average_accuracy"] = classifier.average_accuracy
+        json_dict["average_prediction"] = classifier.average_prediction
+        json_dict["average_recall"] = classifier.average_recall
+        json_dict["average_f1"] = classifier.average_f1
+        json_dict["note"] = classifier.note
+
+        json.dump(json_dict, f)
 
 
 def get_newest_submission_number():
@@ -186,4 +212,4 @@ if __name__ == "__main__":
 
     evaluate(best_classifier.classifier, storage_basename)
 
-    store_classifier(best_classifier.classifier, storage_basename)
+    store_classifier(best_classifier, storage_basename)
